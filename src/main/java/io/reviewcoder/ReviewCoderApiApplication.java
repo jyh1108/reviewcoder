274856f07191c6736c19df9c8c8ee3d3306d8ac3
jyong yong hwa <kobcom12@naver.com>
2025-09-04 00:14:10 +0900

    첫번째 프로젝트 세팅

---
+++ src/main/java/io/reviewcoder/ReviewCoderApiApplication.java
@@ -0,0 +1,13 @@
+package io.reviewcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewCoderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewCoderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/reviewcoder/ReviewCoderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reviewcoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewCoderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
